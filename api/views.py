ba917a187325dfd5c3f4f549535fb9c3769f9622
sujin-park0607 <goweg0607@gmail.com>
2022-05-10 21:19:47 +0900

    modified: signup

--- api/views.py
+++ api/views.py
@@ -1,15 +1,11 @@
-from django.shortcuts import redirect, render, HttpResponseRedirect
 from django.http import JsonResponse
 
 from rest_framework.decorators import api_view, permission_classes
 from rest_framework import permissions
-from rest_framework.response import Response
 from rest_framework.parsers import JSONParser
-from rest_framework.reverse import reverse
 from .serializers import UsersSerializer
-# , MonthSerializer, DaySerializer, TimeSerializer, NowSerializer
 
-from .models import Users  
+from .models import Users,Stay
 
 # Create your views here.
 def login(request):
@@ -33,14 +29,10 @@ def signup(request):
     suc_fal = {'success': "False"}
     return JsonResponse(suc_fal)
 
-# @api_view(['POST'])
-# @permission_classes((permissions.AllowAny,))
+# @api_view(['GET'])
 # def year(request):
-#     data = JSONParser().parse(request)
-#     serializer = YearSerializer(data=data)
-#     serializer.is_valid(raise_exception=True)
-#     serializer.save()
-#     return JsonResponse(serializer.data, safe=False)
+#     data = Stay.objects.get()
+#     return JsonResponse()
 
 # @api_view(['POST'])
 # @permission_classes((permissions.AllowAny,))
